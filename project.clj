352f0f4c2bffc88e561805e3ed7dce3638f37970
Circle CI <circleci@infrablocks.io>
2020-06-18 13:36:38 +0000

    Release version 0.1.1 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/component.jdbc-data-source.postgres "0.1.1-RC5"
+(defproject io.logicblocks/component.jdbc-data-source.postgres "0.1.1"
   :description "A component providing a pooled postgres JDBC data source."
   :url "https://github.com/logicblocks/component.jdbc-data-source.postgres"
 
