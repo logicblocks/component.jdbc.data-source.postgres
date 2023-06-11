b1462d1f1459083c372a9c55293b47223e6fd8bf
Circle CI <circleci@logicblocks.io>
2023-06-11 20:13:28 +0000

    Pre-release version 0.1.3-RC1 [skip ci]

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.logicblocks/component.jdbc-data-source.postgres "0.1.2"
+(defproject io.logicblocks/component.jdbc-data-source.postgres "0.1.3-RC1"
   :description "A component providing a pooled postgres JDBC data source."
   :url "https://github.com/logicblocks/component.jdbc-data-source.postgres"
 
