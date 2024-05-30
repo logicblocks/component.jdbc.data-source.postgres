b13bcc387bd39ab06307fd989df4014ba5a03408
Toby Clemson <tobyclemson@gmail.com>
2024-05-30 13:00:46 +0100

    Use log utilities from component.support.

--- src/component/jdbc/data_source/postgres/component.clj
+++ src/component/jdbc/data_source/postgres/component.clj
@@ -1,43 +1,22 @@
 (ns component.jdbc.data-source.postgres.component
   (:require
-   [cartus.core :as log]
    [com.stuartsierra.component :as component]
-   [component.jdbc.data-source.postgres.data-sources :as data-sources])
-  (:import
-   [clojure.lang Associative]
-   [java.io Closeable]))
-
-(defn- with-logging-fn [logger target opts action-fn]
-  (let [init-ms (System/currentTimeMillis)]
-    (when logger
-      (log/debug logger (keyword (name target)
-                          (name (get-in opts [:phases :before])))
-        (:context opts)))
-    (let [result (action-fn)]
-      (when logger
-        (log/info logger (keyword (name target)
-                           (name (get-in opts [:phases :after])))
-          (merge {:elapsed-ms (- (System/currentTimeMillis) init-ms)}
-            (:context opts))))
-      result)))
-
-(defmacro ^:private with-logging [logger target opts & body]
-  `(with-logging-fn ~logger ~target ~opts
-     (fn [] ~@body)))
+   [component.jdbc.data-source.postgres.data-sources :as data-sources]
+   [component.support.logging :as comp-log]))
 
 (defrecord PostgresJdbcDataSource
   [configuration logger datasource]
 
   component/Lifecycle
   (start [component]
-    (with-logging logger :component.jdbc.data-source.postgres
+    (comp-log/with-logging logger :component.jdbc.data-source.postgres
       {:phases {:before :starting :after :started}
        :context {:configuration configuration}}
       (assoc component
         :datasource (data-sources/postgres-data-source configuration))))
 
   (stop [component]
-    (with-logging logger :component.jdbc.data-source.postgres
+    (comp-log/with-logging logger :component.jdbc.data-source.postgres
       {:phases {:before :stopping :after :stopped}
        :context {:configuration configuration}}
       (assoc component :datasource nil))))
